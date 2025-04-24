54349bfb8e42245a38ae4d38c5eb7340b3266652
Result13 <144160937+Result13@users.noreply.github.com>
2025-04-24 23:37:05 +0700

    Add files via upload

--- clp/js/script.js
+++ clp/js/script.js
@@ -110,5 +110,63 @@ $(document).ready(function () {
 
    
   });
+  $(document).ready(function () {
+    var $slider = $(".cours__wrapper"); // Определяем переменную $slider
+    $(".slider-dots-course").empty();
+    $slider.slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      centerMode: false,
+      variableWidth: false,
+      arrows: false, // Отключаем стандартные стрелки
+      dots: true, // Включаем, чтобы работал `customPaging`
+      appendDots: $(".slider-dots-course"), // Теперь он реально добавит доты
+      customPaging: function (slider, i) {
+        return '<button class="dot-course"></button>'; // Свои доты
+      },
+      responsive: [
+        {
+          breakpoint: 1199,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            /*  centerMode: true, */
+            /*  centerPadding: '-10px'  */
+          },
+        },
+        {
+          breakpoint: 991,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+          },
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+      ],
+    });
+  
+    $(".slider-prev.slider-prev-course").click(function () {
+      $slider.slick("slickPrev");
+    });
+  
+    $(".slider-next.slider-next-course").click(function () {
+      $slider.slick("slickNext");
+    });
+  
+    // Подключаем кастомные доты
+    $(".slider-dots-course").on("click", ".dot-course", function () {
+      let index = $(this).index();
+      $slider.slick("slickGoTo", index);
+    });
+
+   
+  });
 
 
